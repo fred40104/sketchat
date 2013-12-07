3b5c4c4d96bb392d238fd9412ee83bab535ef54e
andyisman <andypcuser@gmail.com>
2013-12-08 01:59:45 +0800

    join and chat/sketch ok

--- node-drawing-game/assets/js/script.js
+++ node-drawing-game/assets/js/script.js
@@ -5,7 +5,7 @@ $(function(){
 		alert('Sorry, it looks like your browser does not support canvas!');
 		return false;
 	}
-
+	console.log('s');
 	// The URL of your web server (the port is set in app.js)
 	var url = 'http://59.127.174.192:8080';
 
@@ -13,8 +13,7 @@ $(function(){
 		win = $(window),
 		canvas = $('#paper'),
 		ctx = canvas[0].getContext('2d'),
-		instructions = $('#instructions');
-	
+		instructions = $('#instructions');	
 	// Generate an unique ID
 	var id = Math.round($.now()*Math.random());
 	
